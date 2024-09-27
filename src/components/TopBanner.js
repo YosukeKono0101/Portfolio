84a4db85528af38d40833c93a9bd7826a761b4c5
Yosuke Kono <n11230380@qut.edu.au>
2024-09-27 16:52:18 +1000

    [CHORE] utilize environment variables for links

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,41 +1,35 @@
 import React from "react";
 import "../main.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import ResumePDF from "../assets/resume.pdf";
 
 const TopBanner = () => {
+  const githubUrl = process.env.REACT_APP_GITHUB_URL;
+  const linkedinUrl = process.env.REACT_APP_LINKEDIN_URL;
+
   return (
-    <div className="top_banner">
-      <div className="jumbotron jumbotron-fluid text-center">
-        <div className="container top">
-          <h1 className="display-4">Yosuke Kono</h1>
-          <p className="lead">Software Developer</p>
-          <div className="social-icons">
-            <a
-              href="https://github.com/YosukeKono0101"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/yosuke-kono-aa12661b0/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-linkedin-in"></i>
-            </a>
-          </div>
-          <div className="cv-download-container">
-            <a
-              href={ResumePDF}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cv-download-button"
-            >
-              View CV
-              <i className="fa-solid fa-eye download-icon"></i>
-            </a>
-          </div>
+    <div id="TopBanner" style={{ marginTop: "56px" }}>
+      <div className="container top text-center">
+        <h1 className="display-4">Yosuke Kono</h1>
+        <p className="lead">Software Developer</p>
+        <div className="social-icons">
+          <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+            <FaGithub size={30} color="white" />
+          </a>
+          <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={30} color="white" />
+          </a>
+        </div>
+        <div className="cv-download-container">
+          <a
+            href={ResumePDF}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cv-download-button"
+          >
+            View CV
+            <i className="fa-solid fa-eye download-icon"></i>
+          </a>
         </div>
       </div>
     </div>
